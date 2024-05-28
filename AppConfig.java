10f785f7d826ae530160992986bb7daaeddfb218
Elvis-321 <83074216+Elvis-321@users.noreply.github.com>
2024-05-28 05:53:52 -0500

    Create AppConfig.java
    Se ha creado esta clase para configurar el Spring creando un bean para el servicio RMI utilizando la clase RmiServiceExporter.

---
+++ AppConfig.java
@@ -0,0 +1,17 @@
+import org.springframework.context.annotation.Bean; 
+import org.springframework.context.annotation.Configuration; 
+import org.springframework.remoting.rmi.RmiServiceExporter; 
+
+@Configuration
+public class AppConfig { 
+
+	@Bean
+	public RmiServiceExporter rmiServiceExporter() { 
+		RmiServiceExporter exporter = new RmiServiceExporter(); 
+		exporter.setServiceName("CalculatorService"); 
+		exporter.setServiceInterface(CalculatorService.class); 
+		exporter.setService(new CalculatorServiceImpl()); 
+		return exporter; 
+	} 
+
+} 
