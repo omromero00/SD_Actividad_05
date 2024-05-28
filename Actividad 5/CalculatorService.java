b49492d0ab0bce27ddb6732f9e0e6b13eb55546d
Gustavo Fernando Aguilar Flores <34445755+gaguilarf@users.noreply.github.com>
2024-05-28 10:59:03 -0500

    Analisis de la Integracion de Spring y RMI
    Proyecto completo

---
+++ Actividad 5/CalculatorService.java
@@ -0,0 +1,8 @@
+// CalculatorService.java 
+import java.rmi.Remote; 
+import java.rmi.RemoteException; 
+
+public interface CalculatorService extends Remote { 
+	public int add(int a, int b) throws RemoteException; 
+	public int subtract(int a, int b) throws RemoteException; 
+} 
---
+++ Actividad 5/CalculatorServiceImpl.java
@@ -0,0 +1,19 @@
+// CalculatorServiceImpl.java 
+import java.rmi.RemoteException; 
+import java.rmi.server.UnicastRemoteObject; 
+
+public class CalculatorServiceImpl extends UnicastRemoteObject implements CalculatorService { 
+
+	public CalculatorServiceImpl() throws RemoteException { 
+		super(); 
+	} 
+
+	public int add(int a, int b) throws RemoteException { 
+		return a + b; 
+	} 
+
+	public int subtract(int a, int b) throws RemoteException { 
+		return a - b; 
+	} 
+} 
+
